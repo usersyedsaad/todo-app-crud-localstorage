baaabff1ccd020fce4a7a934a582b13311de3691
Syed Saad Sohail <saadsohail360@gmail.com>
2025-03-19 02:00:06 +0500

    final commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Counter from "./projects/counter/Counter";
-
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 const Home = () => {
   return (
-    <div>
-      <Counter />
+    <div className="h-screen w-screen flex items-center justify-center flex-col gap-4">
+      <h1 className="text-5xl font-extrabold text-blue-950 line-clamp-6 h-auto p-5">
+        Projects
+      </h1>
+      <Button>
+        <Link to={"/counter"}>Counter App</Link>
+      </Button>
+      <Button>
+        <Link to={"/todo"}>Todo App</Link>
+      </Button>
     </div>
   );
 };

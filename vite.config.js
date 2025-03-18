60731ec71c70bc3d291aa0500e44a5202bad16af
Syed Saad Sohail <saadsohail360@gmail.com>
2025-03-19 01:53:38 +0500

    new push

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+import { fileURLToPath } from "url";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
